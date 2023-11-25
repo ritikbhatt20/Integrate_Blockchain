6aad883222318d172a345f9cd694d8043429bda7
ritik <ritikbhatt020@gmail.com>
2023-11-25 12:49:08 +0530

    Finally started interacting with smart contracts..

--- app/src/main/java/com/example/greeterblockchain/MainActivity.kt
+++ app/src/main/java/com/example/greeterblockchain/MainActivity.kt
@@ -5,56 +5,76 @@ import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.util.concurrent.Future;
 import org.web3j.crypto.Credentials
 import org.web3j.protocol.Web3jFactory
+import org.web3j.protocol.core.RemoteCall
 import org.web3j.protocol.core.methods.response.TransactionReceipt
 import org.web3j.protocol.infura.InfuraHttpService
+import org.web3j.tx.ClientTransactionManager
+import org.web3j.tx.RawTransactionManager
 import java.math.BigInteger
+
 
 
 class MainActivity : AppCompatActivity() {
     private val TAG = javaClass.name
-
+    private lateinit var fab:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab = findViewById(R.id.fab)
+        var greetTextview = findViewById<TextView>(R.id.textView)
 
-        val contractAddress = "0x8394cDf176A4A52DA5889f7a99c4f7AD2BF59088"
-        val url = "https://rinkeby.infura.io/v3/01eb8f7b5e514832af8e827c23784d23"
+//        val chainId = 80001
+        val contractAddress = "0x7b493c1D299650FDf3127DaF2188B24CA797e012"
+        val url = "https://sepolia.infura.io/v3/2ba7c114e14144bbb6d6e827d3547fe3"
         val web3j = Web3jFactory.build(InfuraHttpService(url))
-        val gasLimit: BigInteger = BigInteger.valueOf(20_000_000_000L)
-        val gasPrice: BigInteger = BigInteger.valueOf(4300000)
+//        val gasLimit: BigInteger = BigInteger.valueOf(20_000_000_000L)
+//        val gasPrice: BigInteger = BigInteger.valueOf(43000)
+        val gasLimit: BigInteger = BigInteger.valueOf(20_000_000_000L) // Adjust gas limit here
+        val gasPrice: BigInteger = BigInteger.valueOf(4300000) // Adjust gas price here
 
-        /*** HONOR SYSTEM TEST ETHER ACCOUNT (ಠ_ಠ) ***/
-        val credentials = Credentials.create("f9319fe162c31947c0ca8fd649a536b7ca311b5f210afdc48b62fd7d18ce53e4")
+        val credentials = Credentials.create("7f669b0ff2156a7e5ee7c48ab6f9fe14017d38396a641afef11fede9b06069a5")
+//        val txManager = RawTransactionManager(web3j, credentials, chainId.toByte())
 
-        val greeter = Greeter.load(contractAddress, web3j, credentials, gasLimit, gasPrice)
+        val greeter = Greeter_sol_Greeter.load(contractAddress, web3j, credentials, gasLimit, gasPrice)
 
-        fab.setOnClickListener { _ ->
-            /*** SIMPLE EXECUTION ONLY FOR DEMO PURPOSES.... THIS THREAD INITIALIZATION HERE IS BAD PRACTICE ***/
+        fab.setOnClickListener {
             val thread = Thread(Runnable {
                 try {
+                    val isValid = !greeter.isValid
+                    runOnUiThread {
+                        showToast("Contract is valid: $isValid")
+                    }
 
-                    // check contract validity
-                    Log.d(TAG, " ${greeter.isValid}")
+                    val greeting: Future<TransactionReceipt>? = greeter.greet().sendAsync()
+                    val receipt: TransactionReceipt? = greeting?.get()
+//                    val greetingValue = greeter.processGreetResponse(receipt)
+                    runOnUiThread {
+//                        showToast("Greeting value returned: $receipt")
+//                    }
+                        greetTextview.text = "Greeting value returned: $receipt"
+                    }
 
-                    // read from contract
-                    val greeting: Future<String>? = greeter.greet().sendAsync()
-                    val convertToString: String? = greeting?.get()
-                    Log.d(TAG, "greeting value returned: $convertToString")
-
-                    // write to contract
-                    val transactionReceipt: Future<TransactionReceipt>? = greeter.changeGreeting("Greeting changed from an Android App (ಠ_ಠ) ").sendAsync()
-                    val result = "Successful transaction. Gas used: ${transactionReceipt?.get()?.blockNumber}  ${transactionReceipt?.get()?.gasUsed}"
-                    Log.d(TAG, result)
+                    val transactionReceipt: Future<TransactionReceipt>? =
+                        greeter.changeGreeting("Greeting changed from an Android App").sendAsync()
+                    val result =
+                        "Successful transaction. Gas used: ${transactionReceipt?.get()?.blockNumber}  ${transactionReceipt?.get()?.gasUsed}"
+                    runOnUiThread {
+                        showToast(result)
+                    }
 
                 } catch (e: Exception) {
                     e.printStackTrace()
-                    Log.d(TAG, "error accessing contract: " + e.message)
+                    runOnUiThread {
+                        showToast("Error accessing contract: ${e.message}")
+                    }
                 }
             })
 
@@ -62,4 +82,8 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
+    private fun showToast(message: String) {
+        Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT).show()
+    }
 }
